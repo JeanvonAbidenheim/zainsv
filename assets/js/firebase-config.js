57501907391148970cbddd558c9adfd8405d99d8
Kertcode <zainalzainalll276@gmail.com>
2026-06-17 12:12:29 +0700

    Fix formatting in firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -13,12 +13,12 @@
 // ============================================
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyAOzdVew3EyKd1DEo17hFreYT0sZwEZle0",
-  authDomain:        "zainsv-85b66.firebaseapp.com",
-  projectId:         "zainsv-85b66",
-  storageBucket:     "zainsv-85b66.firebasestorage.app",
+  apiKey: "AIzaSyAOzdVew3EyKd1DEo17hFreYT0sZwEZle0",
+  authDomain: "zainsv-85b66.firebaseapp.com",
+  projectId: "zainsv-85b66",
+  storageBucket: "zainsv-85b66.firebasestorage.app",
   messagingSenderId: "42153126202",
-  appId:             "1:42153126202:web:1db4f7592664c0e8790803",
+  appId: "1:42153126202:web:1db4f7592664c0e8790803",
   measurementId: "G-0JLJFCFLQ8"
 };
 export default firebaseConfig;
